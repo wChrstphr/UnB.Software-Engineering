94f2323215b43584c75e77b753bbcc1754936de2
Christopher <105730846+wChrstphr@users.noreply.github.com>
2023-02-04 00:28:30 -0300

    Projeto final de APC
    Projeto da matéria de Algoritmos e Programação de Computadores (APC) da Faculdade UnB-Campus Gama.
    
    Professor responsável: Giovanni Almeida Santos.
    Proposta de Trabalho: Criação do software "Sistema de Gestao de Beneficio Social (SGBS)".
    Função: Registro de pessoas com benefício social.
    
    Funções do software:
    1- Cadastrar Pessoa;
    2- Consultar Pessoa;
    3- Listar pessoas por Cidade;
    4- Gerar relatorio;
    5- Excluir Pessoa;
    6- Encerrar Programa;

--- main.c
+++ main.c
@@ -1,36 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "222037620.h"
 
 int main() {
     int opcao;
-    
+    int numero;
     
     do {
         limpar_tela();
         menu();
-    
-        scanf("%d", &opcao);
-        limpar_tela();
-        switch (opcao) {
-            case CADASTRAR_PESSOA:
-            cadastrar_pessoa();
-            break;
-            case CONSULTAR_PESSOA:
-            consultar_pessoa();
-            break;
-            case LISTAR_PESSOAS_CIDADE:
-            printf("Not working\n");
-            hub();
-            break;
-            case GERAR_RELATORIO:
-            printf("Not working\n");
-            hub();
-            break;
-            case EXCLUIR_PESSOA:
-            printf("Not working\n");
-            hub();
+        numero = scanf("%d", &opcao);
+        if (numero == 1) {
+            limpar_tela();
+            switch (opcao) {
+                case CADASTRAR_PESSOA:
+                cadastrar_pessoa();
+                break;
+                case CONSULTAR_PESSOA:
+                consultar_pessoa();
+                break;
+                case LISTAR_PESSOAS_CIDADE:
+                listar_pessoas_cidade();
+                break;
+                case GERAR_RELATORIO:
+                gerar_relatorio();
+                hub();
+                break;
+                case EXCLUIR_PESSOA:
+                excluir_pessoa();
+                break;
+            }
+        } else {
+            limpar_tela();
+            printf("Input invalido.");
             break;
         }
     } while (opcao != 6);
